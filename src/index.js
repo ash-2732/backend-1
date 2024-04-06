918698d13016a83e678bfb11753a41364f734371
Abdullah Ashik <121299454+ash-2732@users.noreply.github.com>
2024-04-06 14:04:15 +0600

    connect to mongoose

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config({ path: './env' });  can use but not for production
+
+import dotenv from 'dotenv';
+import connectToDatabase from './db/index.js';
+
+dotenv.config({ path: './env' });
+
+connectToDatabase();
+
+
+
+
+
+
+
+
+
+
+// This another approach to connect to the database and start the server
+
+// const app = express();
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR: ", error)
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is listrning on port ${process.env.PORT}`)
+//         });
+
+//     } catch (error) {
+//         console.log("ERROR: ", error)
+//         throw error;
+//     }
+// } )();
